9fda8f63f73999d1e0b9fe21fc39f0ed0fd39a1a
lucasfri <Lucas.Friesen@gov.bc.ca>
2024-07-17 17:38:57 -0700

    added styler. might break

--- lua/ljfriese/plugins/conform.lua
+++ lua/ljfriese/plugins/conform.lua
@@ -18,14 +18,21 @@ return { -- Autoformat
       -- have a well standardized coding style. You can add additional
       -- languages here or re-enable it for the disabled ones.
       local disable_filetypes = { c = true, cpp = true }
+      local slow_timeout = { r = true, qmd = true }
+      local timeout = 500
+      if slow_timeout[vim.bo[bufnr].filetype] then
+        timeout = 2000
+      end
       return {
-        timeout_ms = 500,
+        timeout_ms = timeout,
         lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
       }
     end,
     formatters_by_ft = {
       lua = { 'stylua' },
       sql = { 'sqlfmt', 'sqlfluff' },
+      r = { 'styler' },
+      qmd = { 'styler' },
     },
   },
 }
