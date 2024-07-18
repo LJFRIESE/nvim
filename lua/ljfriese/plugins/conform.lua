fada1974d93dbcc6eef9442da1c3b279792ff811
lucasfri <Lucas.Friesen@gov.bc.ca>
2024-07-18 11:26:43 -0700

    fixed corruption

---
+++ lua/ljfriese/plugins/conform.lua
@@ -0,0 +1,38 @@
+return { -- Autoformat
+  'stevearc/conform.nvim',
+  lazy = false,
+  keys = {
+    {
+      '<leader>f',
+      function()
+        require('conform').format { async = true, lsp_fallback = true }
+      end,
+      mode = '',
+      desc = '[F]ormat buffer',
+    },
+  },
+  opts = {
+    notify_on_error = true,
+    format_on_save = function(bufnr)
+      -- Disable "format_on_save lsp_fallback" for languages that don't
+      -- have a well standardized coding style. You can add additional
+      -- languages here or re-enable it for the disabled ones.
+      local disable_filetypes = { c = true, cpp = true }
+      local slow_timeout = { r = true, qmd = true }
+      local timeout = 500
+      if slow_timeout[vim.bo[bufnr].filetype] then
+        timeout = 2000
+      end
+      return {
+        timeout_ms = timeout,
+        lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
+      }
+    end,
+    formatters_by_ft = {
+      lua = { 'stylua' },
+      sql = { 'sqlfmt', 'sqlfluff' },
+      r = { 'styler' },
+      quarto = { 'styler' },
+    },
+  },
+}
---
+++ lua/ljfriese/plugins/which-keys.lua
@@ -0,0 +1,35 @@
+return {
+    'folke/which-key.nvim',
+    event = 'VeryLazy',
+    opts = {
+      preset = 'helix',
+      expand = -1,
+      --- Mappings are sorted using configured sorters and natural sort of the keys
+      --- Available sorters:
+      --- * local: buffer-local mappings first
+      --- * order: order of the items (Used by plugins like marks / registers)
+      --- * group: groups last
+      --- * alphanum: alpha-numerical first
+      --- * mod: special modifier keys last
+      --- * manual: the order the mappings were added
+      --- * case: lower-case first
+      sort = { 'group', 'local', 'order', 'alphanum', 'mod' },
+    },
+    keys = {
+      {
+        '<leader>?',
+        function()
+          require('which-key').show { global = false }
+        end,
+        desc = 'Buffer Local Keymaps (which-key)',
+      },
+    },
+  }
+  
+  -- Useful plugin to show you pending keybinds.
+  -- 'folke/which-key.nvim',
+  -- event = 'VimEnter', -- Sets the loading event to 'VimEnter'
+  -- config = function() -- This is the function that runs, AFTER loading
+  --   require('which-key').setup()
+  --
+  
