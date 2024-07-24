4538e9eb76176796a97d7f9bafa8342aca8aa3c5
ljfriese <lucasjfriesen@gmail.com>
2024-07-24 08:59:26 -0700

    fixed trouble, added mini stuff, started bufferlin

--- lua/ljfriese/plugins/mini.lua
+++ lua/ljfriese/plugins/mini.lua
@@ -1,38 +1,68 @@
 return {
-  'echasnovski/mini.nvim',
-  config = function()
-    -- Better Around/Inside textobjects
-    --
-    -- Examples:
-    --  - va)  - [V]isually select [A]round [)]paren
-    --  - yinq - [Y]ank [I]nside [N]ext [']quote
-    --  - ci'  - [C]hange [I]nside [']quote
-    require('mini.ai').setup({ n_lines = 500 })
+  -- { 'echasnovski/mini.nvim', opts = { n_lines = 500 } },
+  {
+    'echasnovski/mini.ai',
+    dependencies = {
+      {
+        'nvim-treesitter/nvim-treesitter-textobjects',
+        init = function()
+          -- no need to load the plugin, since we only need its queries
+          require('lazy.core.loader').disable_rtp_plugin('nvim-treesitter-textobjects')
+        end,
+      },
+    },
+    opts = function()
+      local ai = require('mini.ai')
+      -- Better Around/Inside texto:with:sobjectsbjects
+      --
+      -- Examples:
+      --  - va)  - [V]isually select [A]round [)]paren
+      --  - yinq - [Y]ank [I]nside [N]ext [']quote
+      --  - ci'  - [C]hange [I]nside [']quote
+      return {
+        n_lines = 500,
+        custom_textobjects = {
+          o = ai.gen_spec.treesitter({ -- code block
+            a = { '@block.outer', '@conditional.outer', '@loop.outer' },
+            i = { '@block.inner', '@conditional.inner', '@loop.inner' },
+          }),
+          f = ai.gen_spec.treesitter({ a = '@function.outer', i = '@function.inner' }), -- function
+          c = ai.gen_spec.treesitter({ a = '@class.outer', i = '@class.inner' }), -- class
+          t = { '<([%p%w]-)%f[^<%w][^<>]->.-</%1>', '^<.->().*()</[^/]->$' }, -- tags
+          d = { '%f[%d]%d+' }, -- digits
+          e = { -- Word with case
+            { '%u[%l%d]+%f[^%l%d]', '%f[%S][%l%d]+%f[^%l%d]', '%f[%P][%l%d]+%f[^%l%d]', '^[%l%d]+%f[^%l%d]' },
+            '^().*()$',
+          },
+          -- i = LazyVim.mini.ai_indent, -- indent
+          -- g = LazyVim.mini.ai_buffer, -- buffer
+          u = ai.gen_spec.function_call(), -- u for "Usage"
+          U = ai.gen_spec.function_call({ name_pattern = '[%w_]' }), -- without dot in function name
+        },
+      }
+    end,
+    config = function(_, opts)
+      require('mini.ai').setup(opts)
+    end,
+  },
+  -- Add/delete/replace surroundings (brackets, quotes, etc.)
+  -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
+  -- - sd'   - [S]urround [D]elete [']quotes
+  -- - sr)'  - [S]urround [R]eplace [)] [']
+  { 'echasnovski/mini.surround' },
+  -- Simple and easy statusline.
+  {
+    'echasnovski/mini.statusline',
+    opts = {
+      use_icons = vim.g.have_nerd_font,
 
-    -- Add/delete/replace surroundings (brackets, quotes, etc.)
-    --
-    -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
-    -- - sd'   - [S]urround [D]elete [']quotes
-    -- - sr)'  - [S]urround [R]eplace [)] [']
-    require('mini.surround').setup()
-
-    -- Simple and easy statusline.
-    --  You could remove this setup call if you don't like it,
-    --  and try some other statusline plugin
-    local statusline = require('mini.statusline')
-    -- set use_icons to true if you have a Nerd Font
-    statusline.setup({ use_icons = vim.g.have_nerd_font })
-
-    -- You can configure sections in the statusline by overriding their
-    -- default behavior. For example, here we set the section for
-    -- cursor location to LINE:COLUMN
-    ---@diagnostic disable-next-line: duplicate-set-field
-    statusline.section_location = function()
-      return '%2l:%-2v'
-    end
-
-    -- ... and there is more!
-    --  Check out: https://github.com/echasnovski/mini.nvim
-  end,
+      -- You can configure sections in the statusline by overriding their
+      -- default behavior. For example, here we set the section for
+      -- cursor location to LINE:COLUMN
+      ---@diagnostic disable-next-line: duplicate-set-field
+      section_location = function()
+        return '%2l:%-2v'
+      end,
+    },
+  },
 }
-
--- plugin/keymap.lua
+++ plugin/keymap.lua
@@ -153,16 +153,67 @@ vim.keymap.set('n', '<leader>sn', function()
 end, { desc = '[S]earch [N]eovim files' })
 
 -- Trouble
--- vim.keymap.set('n', '<leader>tt', function()
---   require('trouble').toggle()
--- end, { desc = '[T]oggle [t]rouble' })
--- vim.keymap.set('n', '[t', function()
---   require('trouble').next { skip_groups = true, jump = true }
--- end, { desc = 'Jump to next error' })
--- vim.keymap.set('n', ']t', function()
---   require('trouble').previous { desc = 'Jump to prev error', skip_groups = true, jump = true }
--- e
-
+-- missing-fields and missing-parameters are disabled for lua.
+wk.add({
+  {
+    '[t',
+    function()
+      if require('trouble').is_open() then
+        require('trouble').next({ skip_groups = true, jump = true })
+      else
+        local ok, err = pcall(vim.cmd.cnext)
+        if not ok then
+          vim.notify(err, vim.log.levels.ERROR)
+        end
+      end
+    end,
+    desc = 'Jump to next error',
+  },
+  {
+    ']t',
+    function()
+      if require('trouble').is_open() then
+        require('trouble').prev({ skip_groups = true, jump = true })
+      else
+        local ok, err = pcall(vim.cmd.cprev)
+        if not ok then
+          vim.notify(err, vim.log.levels.ERROR)
+        end
+      end
+    end,
+    desc = 'Jump to prev error',
+  },
+  {
+    '<leader>tt',
+    '<cmd>Trouble diagnostics toggle<cr>',
+    desc = '[T]rouble Diagnostics',
+  },
+  {
+    '<leader>tb',
+    '<cmd>Trouble diagnostics toggle filter.buf=0<cr>',
+    desc = '[B]uffer Diagnostics',
+  },
+  {
+    '<leader>ts',
+    '<cmd>Trouble symbols toggle focus=false<cr>',
+    desc = '[S]ymbols (Trouble)',
+  },
+  {
+    '<leader>td',
+    '<cmd>Trouble lsp toggle focus=false win.position=right<cr>',
+    desc = '[D]efinitions / references',
+  },
+  {
+    '<leader>tl',
+    '<cmd>Trouble loclist toggle<cr>',
+    desc = '[L]ocation List',
+  },
+  {
+    '<leader>tq',
+    '<md>Trouble qflist toggle<cr>',
+    desc = '[Q]uickfix List',
+  },
+})
 -- vim.keymap.set({ 'n', 'x' }, '<leader>y', '"+y', { desc = 'Yank | No clipboard' })
 -- vim.keymap.set({ 'n', 'x' }, '<leader>Y', '"*Y', { desc = 'Yank | Yes clipboard' })
 
