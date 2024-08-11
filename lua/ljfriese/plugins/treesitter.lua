4b125c56e96ef0ca6c20b0bb9b50bbdb8cbf491f
ljfriese <lucasjfriesen@gmail.com>
2024-08-10 18:00:53 -0700

    treesitter context

--- lua/ljfriese/plugins/treesitter.lua
+++ lua/ljfriese/plugins/treesitter.lua
@@ -4,12 +4,9 @@ return { -- Highlight, edit, and navigate code
   build = ':TSUpdate',
   opts = {
     ensure_installed = { 'yaml', 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'vim', 'vimdoc', 'sql', 'r', 'markdown', 'markdown_inline', 'rnoweb', 'sql' },
-    -- Autoinstall languages that are not installed
     auto_install = true,
   },
   config = function(_, opts)
-    -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
-    -- Prefer git instead of curl in order to improve connectivity in some environments
     require('nvim-treesitter.install').prefer_git = true
     ---@diagnostic disable-next-line: missing-fields
     require('nvim-treesitter.configs').setup(opts)
@@ -20,9 +17,7 @@ return { -- Highlight, edit, and navigate code
       line_numbers = true,
       multiline_threshold = 20, -- Maximum number of lines to show for a single context
       trim_scope = 'outer', -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
-      mode = 'topline', -- Line used to calculate context. Choices: 'cursor', 'topline'
-      -- Separator between context and content. Should be a single character string, like '-'.
-      -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
+      mode = 'cursor', -- Line used to calculate context. Choices: 'cursor', 'topline'
       separator = '-',
       zindex = 20, -- The Z-index of the context window
       on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
