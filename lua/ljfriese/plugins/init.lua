20020f436557f7e1ee80a172a9bfca0268abbdea
ljfriese <lucasjfriesen@gmail.com>
2024-08-16 09:04:26 -0700

    asdasdas

--- lua/ljfriese/plugins/init.lua
+++ lua/ljfriese/plugins/init.lua
@@ -1,14 +1,18 @@
 return {
-  { 'eandrju/cellular-automaton.nvim', lazy = true },
+  { 'eandrju/cellular-automaton.nvim'},
   { 'nvim-lua/plenary.nvim' },
   { 'tpope/vim-sleuth' }, -- Detect tabstop and shiftwidth automatically
     { 'brenoprata10/nvim-highlight-colors',
     config = function()
       require("nvim-highlight-colors").turnOn()
+      vim.keymap.set('n', '<leader>c', function()
+          require('nvim-highlight-colors').toggle()
+        end, { desc = 'Toggle [c]olours' })
+
     end},
   {
     'folke/todo-comments.nvim',
     event = 'VimEnter',
     dependencies = { 'nvim-lua/plenary.nvim' },
-    opts = { signs = false },
 }}
+
--- lua/ljfriese/plugins/lsp.lua
+++ lua/ljfriese/plugins/lsp.lua
@@ -4,19 +4,21 @@ return { -- LSP Configuration & Plugins
     {
       'williamboman/mason.nvim',
       config = { ui = { title = 'Mason', border = 'rounded' } },
-    }, -- NOTE: Must be loaded before dependants
+    },
     'williamboman/mason-lspconfig.nvim',
     'WhoIsSethDaniel/mason-tool-installer.nvim',
     { 'j-hui/fidget.nvim', opts = { notification = { window = { border = 'rounded' } } } },
     'hrsh7th/cmp-nvim-lsp',
     { 'ray-x/lsp_signature.nvim' },
+    'https://git.sr.ht/~whynothugo/lsp_lines.nvim',
   },
-  config = function()
-    require('lspconfig.ui.windows').default_options.border = 'rounded'
+  opts = function()
     -- require('lspconfig.ui.windows').default_options.title = LspInfoList
 
+    require('lspconfig.ui.windows').default_options.border = 'rounded'
     -- pop up border
     local orig_util_open_floating_preview = vim.lsp.util.open_floating_preview
+    ---@diagnostic disable-next-line
     function vim.lsp.util.open_floating_preview(contents, syntax, opts, ...)
       opts = opts or {}
       opts.title = 'LSP'
@@ -33,25 +35,27 @@ return { -- LSP Configuration & Plugins
             return 'eol'
           end,
         })
+        vim.opt_local.omnifunc = 'v:lua.vim.lsp.omnifunc'
 
         local map = function(keys, func, desc)
           vim.keymap.set('n', keys, func, { buffer = event.buf, desc = desc })
         end
-
+        local builtin = require('telescope.builtin')
         -- Jump to the definition of the word under your cursor.
         --  To jump back, press <C-t>.
-        map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
         map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-        map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-        map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
-        map('gT', require('telescope.builtin').lsp_type_definitions, '[G]oto [T]ype')
+        map('gd', builtin.lsp_definitions, '[G]oto [D]efinition')
+        map('gr', builtin.lsp_references, '[G]oto [R]eferences')
+        map('gI', builtin.lsp_implementations, '[G]oto [I]mplementation')
+        map('gT', builtin.lsp_type_definitions, '[G]oto [T]ype')
         -- Fuzzy find all the symbols in your current document.
-        map('<leader>syd', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-        map('<leader>syw', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+        map('<leader>syd', builtin.lsp_document_symbols, '[D]ocument [S]ymbols')
+        map('<leader>syw', builtin.lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
         -- Rename the variable under your cursor.
         map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
         map('<leader>gx', vim.lsp.buf.code_action, 'Execute code action')
         map('K', vim.lsp.buf.hover, 'Hover Documentation')
+
         -- The following two autocommands are used to highlight references of the
         -- word under your cursor when your cursor rests there for a little while.
         -- When you move your cursor, the highlights will be cleared (the second autocommand).
@@ -79,6 +83,18 @@ return { -- LSP Configuration & Plugins
           })
         end
 
+        -- Override server capabilities
+        --https://github.com/tjdevries/config.nvim/blob/master/lua/custom/plugins/lsp.lua
+        if client and client.settings.server_capabilities then
+          for k, v in pairs(client.settings.server_capabilities) do
+            if v == vim.NIL then
+              ---@diagnostic disable-next-line: cast-local-type
+              v = nil
+            end
+
+            client.server_capabilities[k] = v
+          end
+        end
         -- The following autocommand is used to enable inlay hints
         if client and client.server_capabilities.inlayHintProvider and vim.lsp.inlay_hint then
           map('<leader><C-h>', function()
@@ -87,28 +103,42 @@ return { -- LSP Configuration & Plugins
         end
       end,
     })
+  end,
+  config = function(_, opts)
     local lsp_flags = {
       allow_incremental_sync = true,
       debounce_text_changes = 150,
     }
+
     local util = require('lspconfig.util')
     local capabilities = vim.lsp.protocol.make_client_capabilities()
+
     capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
     capabilities.textDocument.completion.completionItem.snippetSupport = true
     -- stops quarto preview fighting with nvim
-    capabilities.workspace.didChangeWatchedFiles.dynamicRegistration = false
+    -- capabilities.workspace.didChangeWatchedFiles.dynamicRegistration = false
 
     capabilities.textDocument.foldingRange = { lineFoldingOnly = true }
     local servers = {
-      prettierd = { 'markdown' },
-      html = { 'html' },
-      ruff = { 'python' },
+      prettierd = {
+        filetypes = { 'markdown' },
+      },
+      -- html = { 'html' },
+      ruff = {
+        filetypes = { 'python' },
+      },
       sqls = {
         cmd = { 'sqls', '-config', 'config.yml' },
         filetypes = { 'sql' },
         root_dir = util.root_pattern('config.yml'),
+        server_capabilities = {
+          documentFormattingProvider = false,
+        },
       },
-      marksman = { filetypes = { 'quarto' }, root_dir = util.root_pattern('.git', '.marksman.toml', '_quarto.yml') },
+      marksman = {
+        filetypes = { 'quarto' },
+        root_dir = util.root_pattern('.git', '.marksman.toml', '_quarto.yml'),
+      },
       r_language_server = {
         settings = {
           r = {
@@ -120,23 +150,26 @@ return { -- LSP Configuration & Plugins
         },
       },
       lua_ls = {
+        server_capabilities = {
+          semanticTokensProvider = vim.NIL,
+        },
         settings = {
           Lua = {
-            completion = {
-              callSnippet = 'Replace',
-            },
+            -- completion = {
+            --   callSnippet = 'Replace',
+            -- },
             diagnostics = { disable = { 'missing-parameter', 'missing-fields' } },
           },
         },
       },
     }
 
-    require('mason').setup()
+    require('mason').setup(opts)
     -- You can add other tools here that you want Mason to install
     -- for you, so that they are available from within Neovim.
     local ensure_installed = vim.tbl_keys(servers or {})
     vim.list_extend(ensure_installed, {
-      'html',
+      -- 'html',
       'lua_ls',
       'ruff',
       'sqls',
@@ -156,5 +189,9 @@ return { -- LSP Configuration & Plugins
         end,
       },
     })
+
+    require('lsp_lines').setup()
+    vim.keymap.set('', '<leader>l', require('lsp_lines').toggle, { desc = 'Toggle diagnostic [l]ines' })
+    vim.diagnostic.config({ virtual_text = false })
   end,
 }
