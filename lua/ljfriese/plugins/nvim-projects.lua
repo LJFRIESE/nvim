c8e32bc4abe67127a0eb58b39cc53b79390cc492
ljfriese <lucasjfriesen@gmail.com>
2024-08-20 21:02:21 -0700

    disabled proejcts...

--- lua/ljfriese/plugins/nvim-projects.lua
+++ lua/ljfriese/plugins/nvim-projects.lua
@@ -1,24 +1,9 @@
 return {
-  'coffebar/neovim-project',
-  dependencies = {
-    { 'nvim-lua/plenary.nvim' },
-    { 'nvim-telescope/telescope.nvim', tag = '0.1.*' },
-    { 'Shatur/neovim-session-manager' },
-  },
-  opts = {
-    projects = {
-      '~/git/*',
-      vim.fn.stdpath('config') .. '/*',
-      '~/_daily-notes/*',
-      '~/projects/*',
-    },
-  },
-  init = function()
-    vim.opt.sessionoptions:append('globals') -- save global variables that start with an uppercase letter and contain at least one lowercase letter.
-    vim.keymap.set('n', '<leader>fP', ':Telescope neovim-project discover<cr>', { desc = '[F]ind projects' })
-    vim.keymap.set('n', '<leader>fp', ':Telescope neovim-project history<cr>', { desc = '[.] Recent projects' })
-    -- vim.keymap.set('n', '<leader>Pq', ':NeovimProjectLoadRecent<cr>', { desc = 'open the previous session.' })
-    -- vim.keymap.set('n', '<leader>Px', ':NeovimProjectLoadHist<cr>', { desc = 'opens the project from the history providing a project dir.' })
-    -- vim.keymap.set('n', '<leader>Pt', ':NeovimProjectLoad<cr>', { desc = 'opens the project from all your projects providing a project dir.' })
-  end,
+  -- 'ahmedkhalf/project.nvim',
+  -- opts = {
+  --   silent_chdir = false,
+  -- },
+  -- config = function()
+  --   require('project_nvim').setup({})
+  -- end,
 }
