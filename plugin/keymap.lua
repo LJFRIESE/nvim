00a9c2f00914fb8f8a459e78a220f981d040a436
ljfriese <lucasjfriesen@gmail.com>
2024-08-25 08:08:46 -0700

    nice keymaps

--- plugin/keymap.lua
+++ plugin/keymap.lua
@@ -1,21 +1,23 @@
-
 -- Manipulate text
-vim.keymap.set({ 'n', 'x' }, '<leader>d', '"_d', { desc = '[d]elete | Black hole' })
-vim.keymap.set({ 'n', 'x' }, '<leader>p', [["_dP]], { desc = '[P]aste | Black hole' })
+vim.keymap.set({ 'n', 'x' }, 'x', '"_x', { desc = 'Black hole' })
+vim.keymap.set({ 'n', 'x' }, 'X', '"_X', { desc = 'Black hole' })
+
+vim.keymap.set('n', '<c-p><c-p>', 'O<c-r>"<esc>', { desc = '[P]aste above' })
 vim.keymap.set('n', '<c-p>', 'o<c-r>"<esc>', { desc = '[P]aste below' })
-
-vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv", { desc = 'Move text down' })
-vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv", { desc = 'Move text up' })
-
 
 vim.keymap.set('n', 'ok', 'mzO<esc>`z', { desc = 'Insert linebreak above' })
 vim.keymap.set('n', 'oj', 'mzo<esc>`z', { desc = 'Insert linebreak below' })
 
 vim.keymap.set('n', 'J', 'mzJ`z', { desc = 'Join subsequent line' })
 
-vim.keymap.set('n', '<leader>rg', [[:%s/\\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = '[G]lobal' })
-vim.keymap.set('n', '<leader>rl', [[:s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = '[L]ine' })
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv", { desc = 'Move text down' })
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv", { desc = 'Move text up' })
 
+vim.keymap.set('n', '<leader>rg', [[:%s/\\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = '[G]lobal :s' })
+vim.keymap.set('n', '<leader>rl', [[:s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = '[L]ine :s' })
+
+vim.keymap.set('v', '<', '<gv', { desc = 'Dedent while remaining in visual mode' })
+vim.keymap.set('v', '>', '>gv', { desc = 'Indent while remaining in visual mode' })
 
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Previous [d]iagnostic' })
@@ -29,29 +31,35 @@ vim.keymap.set('n', 'gcl', 'A--no lint<esc>', { desc = 'disable [l]int' })
 
 -- Window navigation
 vim.keymap.set('n', '|', '<c-w>v', { desc = 'Virtical split' })
+
 vim.keymap.set('n', '<leader><', '<C-w>h', { desc = 'Go to left window' })
 vim.keymap.set('n', '<leader>>', '<C-w>l', { desc = 'Go to right window' })
 
 vim.keymap.set('n', 'n', 'nzzzv', { desc = 'Next result' })
 vim.keymap.set('n', 'N', 'Nzzzv', { desc = 'Previous result' })
 
-vim.keymap.set('n', '<c-d>', 'zz<c-d>', {desc = 'Jump down and center'})
-vim.keymap.set('n', '<c-u>', 'zz<c-u>', {desc = 'Jump up and center'})
+vim.keymap.set('n', '<c-d>', 'zz<c-d>', { desc = 'Jump down and center' })
+vim.keymap.set('n', '<c-u>', 'zz<c-u>', { desc = 'Jump up and center' })
 
 -- Misc
 vim.keymap.set('i', '<C-c>', '<Esc>', { desc = 'Esc' })
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>', { desc = 'Kill search highlight' })
-vim.keymap.set({ 'n' }, '<c-c>t', ':split<cr>:terminal<cr>i', { desc = '[t]erminal' })
+vim.keymap.set('n', '<c-c>t', ':split<cr>:terminal<cr>i', { desc = '[t]erminal' })
 vim.keymap.set('t', '<Esc>', '<C-\\><C-n>', { desc = 'Escape terminal' })
--- vim.keymap.set({ 'n' }, '<leader>i', '<esc>i```{r}<cr>```<esc>O', { desc = '[i]nsert code chunk' })
--- Make U opposite to u.
-  vim.keymap.set('n', 'U', '<C-r>', { desc = 'Redo' })
--- Indent while remaining in visual mode.
-vim.keymap.set('v', '<', '<gv')
-vim.keymap.set('v', '>', '>gv')
 
--- Overloaded to toggle nvim-cmp documentation window as well.
-vim.keymap.set({ 'n', 'i' }, '<C-k>', function()
+vim.keymap.set('n', 'U', '<C-r>', { desc = 'Redo' })-- Make U opposite to u.
+
+
+vim.g.cmptoggle = false -- initialize global var to false -> nvim-cmp turned off per default
+local cmp = require('cmp')
+cmp.setup({
+  enabled = function()
+    return vim.g.cmptoggle
+  end,
+})
+vim.keymap.set('n', '<leader>m', '<cmd>lua vim.g.cmptoggle = not vim.g.cmptoggle<CR>', { desc = 'Toggle nvim-cmp' })
+
+vim.keymap.set({ 'n', 'i' }, '<C-k>', function() -- Overloaded to toggle nvim-cmp documentation window as well.
   require('lsp_signature').toggle_float_win()
 end, { silent = true, noremap = true, desc = 'Toggle LSP signature' })
 
@@ -61,7 +69,6 @@ vim.keymap.set('n', '<leader>Z', function()
   vim.wo.number = true
   vim.wo.rnu = true
 end, { desc = '[Z]en mode' })
-
 
 -- Which-key groupings
 local wk = require('which-key')
@@ -75,11 +82,10 @@ wk.add({
   { 'g', group = '[G]o to ...' }, -- , icon = "󰈆 "},
   { 'gs', group = '[s]urrounding ...', icon = '' },
   { '<leader>s', group = '[S]earch ...', icon = '' },
-  { '<leader>t', desc = '[T]rouble'},
+  { '<leader>t', desc = '[T]rouble' },
   { '<leader>f', desc = '[F]ind ...', icon = '' },
   { '<leader>h', group = '[H]arpoon window' },
   { '<leader>b', group = '[B]uffer' },
   { '<leader>r', group = '[R]egex replace' },
   { 'z', group = 'Fold code' },
 })
-
