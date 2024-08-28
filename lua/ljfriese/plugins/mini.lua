4bc8717cddd81a444c3d3c49bba11c525da5b758
ljfriese <lucasjfriesen@gmail.com>
2024-08-27 18:00:43 -0700

    Status line has file tail and session name centered

--- lua/ljfriese/plugins/mini.lua
+++ lua/ljfriese/plugins/mini.lua
@@ -74,10 +74,41 @@ return {
     'echasnovski/mini.statusline',
     opts = {
       use_icons = vim.g.have_nerd_font,
-      ---@diagnostic disable-next-line: duplicate-set-field
-      section_location = function()
-        return '%2l:%-2v'
-      end,
+      content = {
+        active = function()
+          local MiniStatusline = require('mini.Statusline')
+          local MiniSessions = require('mini.Sessions')
+          local get_session = function()
+            local session_name = ''
+            if MiniSessions.config.file ~= nil then
+              session_name = MiniSessions.config.file
+            else
+              session_name = MiniSessions.get_latest()
+            end
+            return session_name
+          end
+
+          local mode, mode_hl = MiniStatusline.section_mode({ trunc_width = 120 })
+          local git = MiniStatusline.section_git({ trunc_width = 40 })
+          local diff = MiniStatusline.section_diff({ trunc_width = 75 })
+          local diagnostics = MiniStatusline.section_diagnostics({ trunc_width = 75 })
+          local lsp = MiniStatusline.section_lsp({ trunc_width = 75 })
+          -- local filename = MiniStatusline.section_filename({ trunc_width = 140 })
+          local fileinfo = MiniStatusline.section_fileinfo({ trunc_width = 120 })
+          local search = MiniStatusline.section_searchcount({ trunc_width = 75 })
+          local session =     get_session()
+
+          return MiniStatusline.combine_groups({
+            { hl = mode_hl, strings = { mode } },
+            { hl = 'MiniStatuslineDevinfo', strings = { git, diff, diagnostics, lsp } },
+            '%<%=', -- Mark general truncate point
+            { hl = 'MiniStatuslineFilename', strings = { '%t' .. ' | ' .. session } },
+            '%=', -- End left alignment
+            { hl = 'MiniStatuslineFileinfo', strings = { fileinfo } },
+            { hl = mode_hl, strings = { search, '%2l:%-2L'} },
+          })
+        end,
+      },
     },
   },
 }
