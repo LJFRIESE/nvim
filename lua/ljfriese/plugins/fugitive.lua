7ba0178cc9770ca6e52af8a31c06825c57f2d670
ljfriese <lucasjfriesen@gmail.com>
2024-08-27 21:48:23 -0700

    fugitive to neogit

--- lua/ljfriese/plugins/fugitive.lua
+++ lua/ljfriese/plugins/fugitive.lua
@@ -1,30 +1,30 @@
 return {
-  event = 'VeryLazy',
-  'tpope/vim-fugitive',
-  config = function()
-    vim.keymap.set('n', 'gt', vim.cmd.Git, { desc = 'Git' })
-    local ljfries_Fugitive = vim.api.nvim_create_augroup('ljfries_Fugitive', {})
-
-    local autocmd = vim.api.nvim_create_autocmd
-    autocmd('BufWinEnter', {
-      group = ljfries_Fugitive,
-      pattern = '*',
-      callback = function()
-        if vim.bo.ft ~= 'fugitive' then
-          return
-        end
-
-        local bufnr = vim.api.nvim_get_current_buf()
-        local opts = { buffer = bufnr, remap = false }
-        vim.keymap.set('n', '<leader>p', function()
-          vim.cmd.Git('push')
-        end, opts)
-
-        -- rebase always
-        vim.keymap.set('n', '<leader>P', function()
-          vim.cmd.Git({ 'pull', '--rebase' })
-        end, opts)
-      end,
-    })
-  end,
+  -- event = 'VeryLazy',
+  -- 'tpope/vim-fugitive',
+  -- config = function()
+  --   vim.keymap.set('n', 'gt', vim.cmd.Git, { desc = 'Git' })
+  --   local ljfries_Fugitive = vim.api.nvim_create_augroup('ljfries_Fugitive', {})
+  --
+  --   local autocmd = vim.api.nvim_create_autocmd
+  --   autocmd('BufWinEnter', {
+  --     group = ljfries_Fugitive,
+  --     pattern = '*',
+  --     callback = function()
+  --       if vim.bo.ft ~= 'fugitive' then
+  --         return
+  --       end
+  --
+  --       local bufnr = vim.api.nvim_get_current_buf()
+  --       local opts = { buffer = bufnr, remap = false }
+  --       vim.keymap.set('n', '<leader>p', function()
+  --         vim.cmd.Git('push')
+  --       end, opts)
+  --
+  --       -- rebase always
+  --       vim.keymap.set('n', '<leader>P', function()
+  --         vim.cmd.Git({ 'pull', '--rebase' })
+  --       end, opts)
+  --     end,
+  --   })
+  -- end,
 }
